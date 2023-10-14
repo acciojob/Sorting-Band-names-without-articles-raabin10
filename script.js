c5a8e98587e4f5661ad12865dba90957bc06b0b7
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-10-14 22:49:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Suppose an array that contains a list of band names
-let bandNames = ['The Beatles', 'Led Zeppelin', 'The Rolling Stones', 'Queen', 'Aerosmith'];
-
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 // Function to remove articles ('a', 'an', 'the') from a band name
 function removeArticles(name) {
   // Regular expression to match articles at the beginning of the string
@@ -9,14 +8,14 @@ function removeArticles(name) {
 }
 
 // Sort the band names in lexicographic order (excluding articles)
-bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+   touristSpotsNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
 
 // Get the ul element with id 'band'
-const bandList = document.getElementById('band');
+const touristSpots = document.getElementById('tourist');
 
 // Create li elements and append them to the ul element
-for (let i = 0; i < bandNames.length; i++) {
+for (let i = 0; i < touristSpots.length; i++) {
   const listItem = document.createElement('li');
-  listItem.textContent = bandNames[i];
-  bandList.appendChild(listItem);
+  listItem.textContent =  touristSpotsNames[i];
+ touristSpots.appendChild(listItem);
 }
